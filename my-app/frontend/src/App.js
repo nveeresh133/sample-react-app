5ec13bcd4e292de299685c30a0bc790d5006633e
veeresh.nagavi <veeresh.nagavi@unilever.com>
2024-08-29 16:13:56 +0530

    up

--- my-app/frontend/src/App.js
+++ my-app/frontend/src/App.js
@@ -14,7 +14,7 @@ const clickPluginConfig = {
 
 const appInsights = new ApplicationInsights({
     config: {
-        connectionString: 'YOUR_CONNECTION_STRING_GOES_HERE',
+        connectionString: 'InstrumentationKey=63f4ae2d-d364-448f-9bbc-842f18e40c24;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=2c935603-2d5f-4911-972b-5f92f01fcfc3', // Replace with your actual connection string
         extensions: [reactPlugin, clickPluginInstance],
         extensionConfig: {
             [reactPlugin.identifier]: { history: browserHistory },
